1609a8d89874f2ff3db97afc77c60f84b8975ef8
andressamorschel <andressamorschel@gmail.com>
2022-03-09 20:07:39 -0300

    first commit

---
+++ producer/src/main/java/com/br/consumer/demo/IntroductionToKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.br.consumer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToKafkaApplication.class, args);
+	}
+
+}
---
+++ producer/src/test/java/com/br/producer/demo/IntroductionToKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.producer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
